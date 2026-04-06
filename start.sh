db848b93ba9982572929717eb2929d103ec78416
btpp03 <btpp03@gmail.com>
2026-04-06 23:03:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,18 +15,18 @@ export TG=${TG:-''}  #配置后节点自动上传TG
 export SUB_URL=${SUB_URL:-''} 
 
 # 哪吒相关设置，支持V0和V1，v1面板搭建教程:https://github.com/dsadsadsss/Docker-for-Nezha-Argo-server-v1.x.git
-export NEZHA_SERVER=${NEZHA_SERVER:-''}  # v0设置域名即可，v1设置域名:端口
+export NEZHA_SERVER=${NEZHA_SERVER:-'nezhak2.btpp.ggff.net'}  # v0设置域名即可，v1设置域名:端口
 export NEZHA_KEY=${NEZHA_KEY:-''}   # v1进后台复制安装命令中的NZ_CLIENT_SECRET值
 export NEZHA_PORT=${NEZHA_PORT:-'443'}  # v1不填这个端口，V1的端口在NEZHA_SERVER中设置
 export NEZHA_TLS=${NEZHA_TLS:-'1'}  # 1启用tls,0关闭tls
 # export AGENT_UUID=${AGENT_UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'}  # 哪吒v1固定的ID，默认随机
 
 # 节点相关设置(节点可在worlds文件里list.log查看)
-export TMP_ARGO=${TMP_ARGO:-'ech'}  # 节点类型,可选vls,vms,spl,xhttp,rel,hy2,tuic，sock,3x
+export TMP_ARGO=${TMP_ARGO:-'3x'}  # 节点类型,可选vls,vms,spl,xhttp,rel,hy2,tuic，sock,3x
 export VL_PORT=${VL_PORT:-'8002'} #vles 端口
 export VM_PORT=${VM_PORT:-'8001'} #vmes 端口
 export CF_IP=${CF_IP:-'ip.sb'}  # cf优选域名或ip
-export SUB_NAME=${SUB_NAME:-'argo'} # 节点名称，配合哪吒面板v1可以自动设置面板名称
+export SUB_NAME=${SUB_NAME:-'wisp-btpp05'} # 节点名称，配合哪吒面板v1可以自动设置面板名称
 export second_port=${second_port:-''} # 可选，第二端口，部分玩具支持设置第二端口
 #export UUID=${UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'} # 设置节点固定的UUID,否则使用随机UUID
 
