e8453b184dc939f43deceaf1e639c7a1394f8feb
btpp03 <btpp03@gmail.com>
2026-04-06 23:15:10 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ export SUB_URL=${SUB_URL:-'https://node.btpp4.netlib.re/token=123456nb?cf_ip=ip.
 
 # 哪吒相关设置，支持V0和V1，v1面板搭建教程:https://github.com/dsadsadsss/Docker-for-Nezha-Argo-server-v1.x.git
 export NEZHA_SERVER=${NEZHA_SERVER:-'nezhak2.btpp.ggff.net'}  # v0设置域名即可，v1设置域名:端口
-export NEZHA_KEY=${NEZHA_KEY:-''}   # v1进后台复制安装命令中的NZ_CLIENT_SECRET值
+export NEZHA_KEY=${NEZHA_KEY:-'LxOLxrvpD4H7SyDzzW'}   # v1进后台复制安装命令中的NZ_CLIENT_SECRET值
 export NEZHA_PORT=${NEZHA_PORT:-'443'}  # v1不填这个端口，V1的端口在NEZHA_SERVER中设置
 export NEZHA_TLS=${NEZHA_TLS:-'1'}  # 1启用tls,0关闭tls
 # export AGENT_UUID=${AGENT_UUID:-'9e0da28d-ee9c-4fef-95a4-df2d0335e649'}  # 哪吒v1固定的ID，默认随机
